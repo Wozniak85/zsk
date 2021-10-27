86f63b0a2dfef5e28b5d82523dc1e925e2e45c34
Wozniak85 <90185200+Wozniak85@users.noreply.github.com>
2021-10-27 15:42:23 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,41 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Worker {
+class Rectangle{
+	private:
+		double sideA, sideB;
 	public:
-		int id {8};
-		string name {"Katarzyna"};
-		string surname {"Nowak"};
-		
-		Worker();
-		Worker(int pId=1, string pName="Name", string pSurname="Surname");
-		
-		void getData();
+		Rectangle();
+		Rectangle(double sideA, double sideB);
+		//prototyp konstruktora kopiujacego
+		Rectangle(const Rectangle &);
 };
 
-	Worker::Worker(){
-		id=13;
-		
-	}
 
-	Worker::Worker(int pId, string pName, string pSurname){
-		id=pId;
-		name=pName;
-		surname=pSurname;
-	}
 
-	void Worker::getData(){
-		cout<<"id: "<<id<<"\nImiê: "<<name<<" "<<"Nazwisko: "<<surname<<endl;
-	}
-
-int main(int argc, char** argv) {
-	setlocale(LC_CTYPE, "polish");
+int main(int argc, char** argv){
+	setlocale(LC_CTYPE, polish)	
 	
-	Worker nowak2 = Worker();
-	nowak2.getData();
-	
-	Worker nowak = Worker();
-	nowak.getData();
-	Worker nowak1 = Worker(10,"Name", "Surname");
 	return 0;
 }
+
